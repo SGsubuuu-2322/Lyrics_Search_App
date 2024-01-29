ba99ee6608b6c550404f7962ade951b43e15867a
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-01-30 02:26:00 +0530

    Added the scripts for the prev and next buttons and optimised way to show the song list onto the DOM...

--- script.js
+++ script.js
@@ -6,14 +6,61 @@ const moreEl = document.getElementById("more");
 const apiUrl = "https://api.lyrics.ovh";
 
 async function searchSong(term) {
-    const res = await fetch(`${apiUrl}/suggest/${term}`);
-    const data = res.json();
+  const res = await fetch(`${apiUrl}/suggest/${term}`);
+  const data = await res.json();
 
-    showData(data);
+  showData(data);
 }
 
-function showData(data){
+function showData(data) {
+  //   let output = "";
 
+  //   data.data.forEach((song) => {
+  //     output += `
+
+  //     <li>
+  //         <span>
+  //             <strong>${song.artist.name}</strong> - ${song.title}
+  //         </span>
+
+  //         <button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}">Get Lyrics</button>
+  //     </li>
+
+  //     `;
+  //   });
+
+  //   resultEl.innerHTML = `
+  //     <ul class="songs">
+  //         ${output}
+  //     </ul>
+  //   `;
+
+  resultEl.innerHTML = `
+    <ul class="songs">
+    ${data.data
+      .map(
+        (song) => `
+          <li>
+                   <span>
+                       <strong>${song.artist.name}</strong> - ${song.title}
+                   </span>
+        
+                  <button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}">Get Lyrics</button>
+               </li>
+          `
+      )
+      .join("")}
+    </ul>
+
+ `;
+
+  if (data.prev || data.next) {
+    moreEl.innerHTML = `
+    ${data.prev ? `<button class="btn">Prev</button>` : ""}
+    ${data.next ? `<button class="btn">Next</button>` : ""}
+    
+    `;
+  }
 }
 
 form.addEventListener("submit", (e) => {
