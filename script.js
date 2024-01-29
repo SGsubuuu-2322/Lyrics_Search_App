7fbd69031b01c4dd67c83dee07ec0520cb88d5b9
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-01-30 01:31:11 +0530

    Added the scripts for fetching the data from that API...

--- script.js
+++ script.js
@@ -5,9 +5,25 @@ const moreEl = document.getElementById("more");
 
 const apiUrl = "https://api.lyrics.ovh";
 
+async function searchSong(term) {
+    const res = await fetch(`${apiUrl}/suggest/${term}`);
+    const data = res.json();
+
+    showData(data);
+}
+
+function showData(data){
+
+}
+
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
   const searchTerm = SearchEl.value.trim();
-  console.log(searchTerm);
+
+  if (!searchTerm) {
+    alert("Please enter in a song or a artist name...");
+  } else {
+    searchSong(searchTerm);
+  }
 });
